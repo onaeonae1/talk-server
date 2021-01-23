eed6b92c4ee0558d40f8b504f9413b5c56589bc2
onaeonae1 <41908963+onaeonae1@users.noreply.github.com>
2021-01-23 20:41:18 +0900

    need to fix lint problem

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,34 +3,34 @@ import Room from '../models/Room';
 import Chat from '../models/Chat';
 
 export const getUsers = async (req, res) => {
-	console.log('get Users');
-	try {
-		const users = await User.find({}).sort({ _id: -1 });
-		res.send(users);
-	} catch (error) {
-		console.log(error.stack);
-		res.status(400).send('Failed to get Users');
-	}
+  console.log('get Users');
+  try {
+    const users = await User.find({}).sort({ _id: -1 });
+    res.send(users);
+  } catch (error) {
+    console.log(error.stack);
+    res.status(400).send('Failed to get Users');
+  }
 };
 export const getRooms = async (req, res) => {
-	console.log('get rooms');
-	try {
-		const rooms = await Room.find({}).sort({ _id: -1 });
-		res.send(rooms);
-	} catch (error) {
-		console.log(error.stack);
-		res.status(400).send('Failed to get Rooms');
-	}
+  console.log('get rooms');
+  try {
+    const rooms = await Room.find({}).sort({ _id: -1 });
+    res.send(rooms);
+  } catch (error) {
+    console.log(error.stack);
+    res.status(400).send('Failed to get Rooms');
+  }
 };
 export const getChats = async (req, res) => {
-	console.log('get chats');
-	try {
-		const chats = await Chat.find({}).sort({ _id: -1 });
-		res.send(chats);
-	} catch (error) {
-		console.log(error.stack);
-		res.status(400).send('Failed to get Chats');
-	}
+  console.log('get chats');
+  try {
+    const chats = await Chat.find({}).sort({ _id: -1 });
+    res.send(chats);
+  } catch (error) {
+    console.log(error.stack);
+    res.status(400).send('Failed to get Chats');
+  }
 };
 
 export const getUserInfo = async (req, res) => {
@@ -39,15 +39,7 @@ export const getUserInfo = async (req, res) => {
   console.log(userId);
   try {
     const user = await User.findOne({ _id: userId });
-    const modified = {
-      _id: user._id,
-      userName: user.userNAme,
-      email: user.email,
-      nickName: user.nickName,
-      avatarUrl: user.avatarUrl,
-      backgroundUrl: user.backgroundUrl,
-      quoteMessage: user.quoteMessage,
-    };
+    const modified = user.getInfo();
     res.send(modified);
   } catch (error) {
     console.log(error.stack);
--- models/User.js
+++ models/User.js
@@ -4,7 +4,6 @@ import bcrypt from 'bcryptjs';
 
 // Schema 정의
 const UserSchema = new mongoose.Schema({
-<<<<<<< HEAD
   userName: {
     type: String,
     required: 'String is required',
@@ -59,70 +58,15 @@ const UserSchema = new mongoose.Schema({
       ref: 'User',
     },
   ],
-=======
-	userName: {
-		type: String,
-		required: 'String is required',
-	},
-	dmMap: {
-		type: Map,
-		of: mongoose.Schema.Types.ObjectId,
-	},
-	// hashedPassword: {
-	// 	type: String,
-	// 	required: 'hashed password is required',
-	// },
-	isConnecting: {
-		type: Boolean,
-		default: false,
-	},
-	nickName: {
-		type: String,
-	},
-	birthDay: {
-		type: Date,
-	},
-	email: {
-		type: String,
-		required: 'Email is Required',
-	},
-	avatarUrl: {
-		type: String,
-	},
-	backgroundUrl: {
-		type: String,
-	},
-	quoteMessage: {
-		type: String,
-	},
-	friendsList: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-		},
-	],
-	roomList: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Room',
-		},
-	],
-	blockList: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-		},
-	],
->>>>>>> 9664724bb23f8c405820321e0003e9b351e66b49
 });
 // Schema Methods
 UserSchema.methods.setPassword = async (password) => {
-	const hash = await bcrypt.hash(password, 10);
-	this.hashedPassword = hash;
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
 };
 UserSchema.methods.checkPassword = async function (password) {
-	const result = await bcrypt.compare(password, this.hashedPassword);
-	return result;
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result;
 };
 UserSchema.methods.getInfo = async function () {
   // 다른 사용자가 조회할 수 있는 부분
@@ -138,8 +82,8 @@ UserSchema.methods.getInfo = async function () {
   return ret;
 };
 UserSchema.statics.findByEmail = async function (email) {
-	const result = await this.findOne({ email });
-	return result;
+  const result = await this.findOne({ email });
+  return result;
 };
 const model = mongoose.model('User', UserSchema);
 export default model;
