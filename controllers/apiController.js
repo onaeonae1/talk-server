d25d14dfe71e579310261ee350cda5ba96caa348
unknown <checkpoint1209@gmail.com>
2021-01-23 15:50:27 +0900

    t

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,32 +3,32 @@ import Room from '../models/Room';
 import Chat from '../models/Chat';
 
 export const getUsers = async (req, res) => {
-  console.log('get Users');
-  try {
-    const users = await User.find({}).sort({ _id: -1 });
-    res.send(users);
-  } catch (error) {
-    console.log(error.stack);
-    res.status(400).send('Failed to get Users');
-  }
+	console.log('get Users');
+	try {
+		const users = await User.find({}).sort({ _id: -1 });
+		res.send(users);
+	} catch (error) {
+		console.log(error.stack);
+		res.status(400).send('Failed to get Users');
+	}
 };
 export const getRooms = async (req, res) => {
-  console.log('get rooms');
-  try {
-    const rooms = await Room.find({}).sort({ _id: -1 });
-    res.send(rooms);
-  } catch (error) {
-    console.log(error.stack);
-    res.status(400).send('Failed to get Rooms');
-  }
+	console.log('get rooms');
+	try {
+		const rooms = await Room.find({}).sort({ _id: -1 });
+		res.send(rooms);
+	} catch (error) {
+		console.log(error.stack);
+		res.status(400).send('Failed to get Rooms');
+	}
 };
 export const getChats = async (req, res) => {
-  console.log('get chats');
-  try {
-    const chats = await Chat.find({}).sort({ _id: -1 });
-    res.send(chats);
-  } catch (error) {
-    console.log(error.stack);
-    res.status(400).send('Failed to get Chats');
-  }
+	console.log('get chats');
+	try {
+		const chats = await Chat.find({}).sort({ _id: -1 });
+		res.send(chats);
+	} catch (error) {
+		console.log(error.stack);
+		res.status(400).send('Failed to get Chats');
+	}
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,111 +1,114 @@
 import User from '../models/User';
 
 export const getUser = async (req, res) => {
-  console.log('getUser');
-  const userId = req.query.id;
-  try {
-    const user = await User.findOne({ _id: userId })
-      .populate('friendsList')
-      .populate('roomList');
-    if (!user) {
-      throw Error('there is no user');
-    }
-    res.send(user);
-  } catch (error) {
-    console.log(error.stack);
-    res.status(400).send('Failed to get User');
-  }
+	console.log('getUser');
+	const userId = req.query.id;
+	try {
+		const user = await User.findOne({ _id: userId })
+			.populate({
+				path: 'roomList',
+				populate: {
+					path: 'userList',
+				},
+			})
+			.populate('friendsList');
+		if (!user) {
+			throw Error('there is no user');
+		}
+		res.send(user);
+	} catch (error) {
+		console.log(error.stack);
+		res.status(400).send('Failed to get User');
+	}
 };
 
 export const addFriend = async (req, res) => {
-  console.log('Add Friend');
-  try {
-    const {
-      body: { userId, friendId },
-    } = req;
-    const targetUser = await User.findOne({ _id: userId });
-    if (targetUser.friendsList.includes(friendId)) {
-      throw Error('they are already friend');
-    } else {
-      targetUser.friendsList.push(friendId);
-      await targetUser.save();
-      res.redirect('/api/getUsers');
-    }
-  } catch (error) {
-    console.log(error.stack);
-    res.status(400).send('Failed to add Friend');
-  }
+	console.log('Add Friend');
+	try {
+		const {
+			body: { userId, friendId },
+		} = req;
+		const targetUser = await User.findOne({ _id: userId });
+		if (targetUser.friendsList.includes(friendId)) {
+			throw Error('they are already friend');
+		} else {
+			targetUser.friendsList.push(friendId);
+			await targetUser.save();
+			res.redirect('/api/getUsers');
+		}
+	} catch (error) {
+		console.log(error.stack);
+		res.status(400).send('Failed to add Friend');
+	}
 };
 export const removeFriend = async (req, res) => {
-  console.log('Removing Friend');
-  try {
-    const {
-      body: { userId, friendId },
-    } = req;
-    const targetUser = await User.findOne({ _id: userId });
-    if (await User.findOne({ _id: friendId })) {
-      console.log(`deleting friend : ${friendId}`);
-      if (targetUser.friendsList.includes(friendId)) {
-        targetUser.friendsList.remove(friendId);
-        await targetUser.save();
-        res.redirect('/api/getUsers');
-      } else {
-        throw Error('they are not friend');
-      }
-    } else {
-      throw Error('failed to find friend');
-    }
-  } catch (error) {
-    console.log(error.stack);
-    res.status(400).send('Failed to remove friend');
-  }
+	console.log('Removing Friend');
+	try {
+		const {
+			body: { userId, friendId },
+		} = req;
+		const targetUser = await User.findOne({ _id: userId });
+		if (await User.findOne({ _id: friendId })) {
+			console.log(`deleting friend : ${friendId}`);
+			if (targetUser.friendsList.includes(friendId)) {
+				targetUser.friendsList.remove(friendId);
+				await targetUser.save();
+				res.redirect('/api/getUsers');
+			} else {
+				throw Error('they are not friend');
+			}
+		} else {
+			throw Error('failed to find friend');
+		}
+	} catch (error) {
+		console.log(error.stack);
+		res.status(400).send('Failed to remove friend');
+	}
 };
 export const blockUser = async (req, res) => {
-  console.log('Block User');
-  try {
-    const {
-      body: { userId, blockId },
-    } = req;
-    const targetUser = await User.findOne({ _id: userId });
-    if (await User.findOne({ _id: blockId })) {
-      if (targetUser.blockList.includes(blockId)) {
-        throw Error('already blocked');
-      } else {
-        targetUser.blockList.push(blockId);
-        await targetUser.save();
-        res.redirect('/api/getUsers');
-      }
-    } else {
-      throw Error('there is no such user');
-    }
-  } catch (error) {
-    console.log(error.stack);
-    res.status(400).send('Failed to block User');
-  }
+	console.log('Block User');
+	try {
+		const {
+			body: { userId, blockId },
+		} = req;
+		const targetUser = await User.findOne({ _id: userId });
+		if (await User.findOne({ _id: blockId })) {
+			if (targetUser.blockList.includes(blockId)) {
+				throw Error('already blocked');
+			} else {
+				targetUser.blockList.push(blockId);
+				await targetUser.save();
+				res.redirect('/api/getUsers');
+			}
+		} else {
+			throw Error('there is no such user');
+		}
+	} catch (error) {
+		console.log(error.stack);
+		res.status(400).send('Failed to block User');
+	}
 };
 export const changeProfile = async (req, res) => {
-  console.log('Change Profile');
-  try {
-    const {
-      body: {
-        userId, avatarUrl, backgroundUrl, nickName, quoteMessage,
-      },
-    } = req;
-    const updateUser = await User.findByIdAndUpdate(userId, {
-      avatarUrl,
-      backgroundUrl,
-      nickName,
-      quoteMessage,
-    });
-    console.log(`Successfully Changed Profile : ${updateUser.nickName}`);
-  } catch (error) {
-    console.log(error.stack);
-    res.status(400).send('Failed to change Profile');
-  }
+	console.log('Change Profile');
+	try {
+		const {
+			body: { userId, avatarUrl, backgroundUrl, nickName, quoteMessage },
+		} = req;
+		const updateUser = await User.findByIdAndUpdate(userId, {
+			avatarUrl,
+			backgroundUrl,
+			nickName,
+			quoteMessage,
+		});
+		console.log(`Successfully Changed Profile : ${updateUser.nickName}`);
+	} catch (error) {
+		console.log(error.stack);
+		res.status(400).send('Failed to change Profile');
+	}
 };
 export const searchUser = async (req, res) => {
-  // 유저 중에서 검색
+	// 유저 중에서 검색
 };
 export const searchFriend = async (req, res) => {
-  // 친구 중에서 검색
+	// 친구 중에서 검색
 };
